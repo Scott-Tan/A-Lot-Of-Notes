8dc1dce10396e22a51aa468adf22f030693c2413
Scott <sctan6688@gmail.com>
2018-10-26 01:21:52 -0700

    Adding some more basic implementations of methods
    need to define queries in get methods

--- app/src/main/java/com/example/a_lot_of_notes/a_lot_of_notes/Database/Database.java
+++ app/src/main/java/com/example/a_lot_of_notes/a_lot_of_notes/Database/Database.java
@@ -10,6 +10,7 @@ package com.example.a_lot_of_notes.a_lot_of_notes.Database;
 *
 * */
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -23,14 +24,18 @@ public class Database extends SQLiteOpenHelper{
     // TAG for debugging
     private static final String TAG = "Database";
 
+    // Variables for database
     private static final String DB_NAME = "notes_db";
     private static final int DB_VERSION = 1;
     // possibly replace name defined in Notes.class
     // these columns are just dummies, replace with definitions in Note.class
     private static final String DB_TABLE_NAME = "table_name";
+    private static final String COLUMN_ID = "_id";
     private static final String COLUMN_DIRECTORY = "directory";
     private static final String COLUMN_PROJECT = "project";
     private static final String COLUMN_NOTES = "notes";
+    private static final String COLUMN_NOTES_TTLE = "notes_title";
+    private static final String COLUMN_NOTES_CONTENT = "notes_content";
 
 
     Context cntxt;
@@ -41,9 +46,22 @@ public class Database extends SQLiteOpenHelper{
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
         Log.d(TAG, "onCreate: starting database creation");
 
+        // String to make define database
+        // First string to initialize a database with the dummy columns
+        //  When creating db with Notes.class, may need some revision.
+        String CREATE_TABLE = "CREATE TABLE " + DB_TABLE_NAME + " ("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_DIRECTORY+ " TEXT, "
+                + COLUMN_PROJECT + " TEXT, "
+                + COLUMN_NOTES + " TEXT, "
+                + COLUMN_NOTES_TTLE + " TEXT, "
+                + COLUMN_NOTES_CONTENT + " TEXT" + ")";
+
+        // Exec SQL database
+        db.execSQL(CREATE_TABLE);
 
         Log.d(TAG, "onCreate: ending database creation");
     }
@@ -54,19 +72,61 @@ public class Database extends SQLiteOpenHelper{
         onCreate(db);
     }
     
-    public void insertData(){
+    public boolean insertData(String notes, String title, String content){
         Log.d(TAG, "insertData: starting");
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        // what should we cv put?
+        // How to handle different additions to database? whether a new directory or note?
+
+        // maybe start by assuming that only notes will be add first before directories
+        cv.put(COLUMN_NOTES, notes);
+        cv.put(COLUMN_NOTES_TTLE, title);
+        cv.put(COLUMN_NOTES_CONTENT, content);
+
+        Log.d(TAG, "insertData: before inserting content values into database");
+        db.insert(DB_TABLE_NAME, null, cv);
+        Log.d(TAG, "insertData: after inserting content values into database");
 
         Log.d(TAG, "insertData: ending");
+        return true;
     }
-    
-    public Cursor getData(){
+
+
+    // A query to get all notes. May need separate queries to get notes in specific directory, or
+    //  from a specific project.
+    public Cursor getAllNotes(){
         Log.d(TAG, "getData: starting");
         SQLiteDatabase db = getReadableDatabase();
         String query = "";
         Cursor data = db.rawQuery(query, null);
 
         Log.d(TAG, "getData: ending");
+        return data;
+    }
+
+
+    // define queries to get needs
+    public Cursor getNotesFromDirectory(){
+        Log.d(TAG, "getNotesFromDirectory: starting");
+        SQLiteDatabase db = getReadableDatabase();
+        String query = "";
+        Cursor data = db.rawQuery(query, null);
+
+        Log.d(TAG, "getNotesFromDirectory: ending");
+        return data;
+    }
+
+
+    // define query to meet needs
+    public Cursor getNotesFromProject(){
+        Log.d(TAG, "getNotesFromProject: starting");
+        SQLiteDatabase db = getReadableDatabase();
+        String query = "";
+        Cursor data = db.rawQuery(query, null);
+
+        Log.d(TAG, "getNotesFromProject: ending");
         return data;
     }
 }
