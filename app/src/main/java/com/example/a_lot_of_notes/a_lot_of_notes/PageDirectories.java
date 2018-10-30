c2bd1b7238136f035f68169440d8a8c9b6bbbebb
Scott <sctan6688@gmail.com>
2018-10-30 12:59:39 -0700

    PageDirectories refactoring.
    Adding a new directory is done through a dialog box. Will remove AddDirectories.class later.

--- app/src/main/java/com/example/a_lot_of_notes/a_lot_of_notes/PageDirectories.java
+++ app/src/main/java/com/example/a_lot_of_notes/a_lot_of_notes/PageDirectories.java
@@ -1,15 +1,20 @@
 package com.example.a_lot_of_notes.a_lot_of_notes;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -20,6 +25,7 @@ import java.util.ArrayList;
 
 public class PageDirectories extends AppCompatActivity {
     private static final String TAG = "PageDirectories";
+    Context ctx;
     Database db;
     private ListView mListView;
     FloatingActionButton fab;
@@ -31,6 +37,7 @@ public class PageDirectories extends AppCompatActivity {
         setContentView(R.layout.page_directories);
 
         Log.d(TAG, "onCreate: starting ");
+        ctx = this;
         db = new Database(this);
         mListView = findViewById(R.id.list_view);
         fab = findViewById(R.id.page_directories_fab);
@@ -40,8 +47,39 @@ public class PageDirectories extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent create_directory = new Intent(getApplicationContext(), AddDirectory.class);
-                startActivity(create_directory);
+                //Intent create_directory = new Intent(getApplicationContext(), AddDirectory.class);
+                //startActivity(create_directory);
+
+                LayoutInflater layoutInflater = LayoutInflater.from(ctx);
+                View mView = layoutInflater.inflate(R.layout.input_dialog_box, null);
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(ctx);
+                alertDialog.setView(mView);
+
+                final EditText userInput = mView.findViewById(R.id.userInputDialog);
+
+                alertDialog
+                        .setCancelable(false)
+                        .setPositiveButton("Create", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                String directory_name = userInput.getText().toString();
+
+                                Log.d(TAG, "onClick: before insertDirectory");
+                                db.insertDirectory(directory_name);
+                                db.close();
+
+                                populateListView();
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        });
+
+                AlertDialog alertDialogAndroid = alertDialog.create();
+                alertDialogAndroid.show();
             }
         });
 
@@ -49,7 +87,7 @@ public class PageDirectories extends AppCompatActivity {
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getApplicationContext(),
+                Toast.makeText(ctx,
                         "This is directory " + listData.get(i) + ". Lead this to projects" +
                                 " with the directory tag.",
                         Toast.LENGTH_LONG).show();
@@ -57,6 +95,7 @@ public class PageDirectories extends AppCompatActivity {
 
             }
         });
+
 
         Log.d(TAG, "onCreate: ending");
 
@@ -86,7 +125,6 @@ public class PageDirectories extends AppCompatActivity {
         ListAdapter adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, listData);
         mListView.setAdapter(adapter);
-
 
         Log.d(TAG, "populateListView: ending");
     }
