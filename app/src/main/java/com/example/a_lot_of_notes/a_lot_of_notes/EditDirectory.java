3cd39e6084b0c2f1cd8ee65ba259870268c6cfbf
rakurnia <44179762+rakurnia@users.noreply.github.com>
2018-10-31 20:42:25 -0700

    Create EditDirectory.java
    Show dialog to edit directory name

---
+++ app/src/main/java/com/example/a_lot_of_notes/a_lot_of_notes/EditDirectory.java
@@ -0,0 +1,70 @@
+package com.example.a_lot_of_notes.a_lot_of_notes;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.a_lot_of_notes.a_lot_of_notes.Database.Database;
+
+public class EditDirectory extends AppCompatDialogFragment {
+
+    private EditText editDirName;
+    private EditDirectoryListener listener;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.edit_directory, null);
+        editDirName = view.findViewById(R.id.edit_directory);
+
+        Bundle bundle = getArguments();
+        final String oldDirName = bundle.getString("oldName");
+
+        editDirName.setText(oldDirName);
+
+        builder.setView(view)
+                .setTitle("Edit Directory")
+                .setNegativeButton("Cancle", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                })
+                .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String newDirName = editDirName.getText().toString();
+                        listener.updateName(newDirName, oldDirName);
+                    }
+                });
+
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            listener = (EditDirectoryListener) context;
+        } catch (ClassCastException e){
+            throw new ClassCastException(context.toString() +
+            "must implement EditDirectoryListener");
+        }
+    }
+
+    public interface EditDirectoryListener{
+        void updateName(String newName, String oldName);
+    }
+
+}
