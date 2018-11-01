e9f2638f2ece384fd14f15c4d62b552ed022722a
rakurnia <44179762+rakurnia@users.noreply.github.com>
2018-10-31 20:36:39 -0700

    Update PageDirectories.java
    Add edit/delete feature to PageDirectories

--- app/src/main/java/com/example/a_lot_of_notes/a_lot_of_notes/PageDirectories.java
+++ app/src/main/java/com/example/a_lot_of_notes/a_lot_of_notes/PageDirectories.java
@@ -10,125 +10,154 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ExpandableListView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
 import com.example.a_lot_of_notes.a_lot_of_notes.Database.Database;
+import com.example.a_lot_of_notes.a_lot_of_notes.model.Directories;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-public class PageDirectories extends AppCompatActivity {
+public class PageDirectories extends AppCompatActivity
+        implements EditDirectory.EditDirectoryListener {
+
     private static final String TAG = "PageDirectories";
-    Context ctx;
+
     Database db;
-    private ListView mListView;
-    FloatingActionButton fab;
-    ArrayList<String> listData;
+    ListView listDirectory;
+    ArrayList<String> directoryData;
+    String newDirName;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_directories);
 
-        Log.d(TAG, "onCreate: starting ");
-        ctx = this;
+        Log.d(TAG, "onCreate: starting");
+
+        // Change action bar title
+        setTitle("List of Directory");
+
         db = new Database(this);
-        mListView = findViewById(R.id.list_view);
-        fab = findViewById(R.id.page_directories_fab);
+        listDirectory = findViewById(R.id.list_directory);
 
-        populateListView();
+        populateDirectoryList();
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Intent create_directory = new Intent(getApplicationContext(), AddDirectory.class);
-                //startActivity(create_directory);
-
-                LayoutInflater layoutInflater = LayoutInflater.from(ctx);
-                View mView = layoutInflater.inflate(R.layout.input_dialog_box, null);
-                AlertDialog.Builder alertDialog = new AlertDialog.Builder(ctx);
-                alertDialog.setView(mView);
-
-                final EditText userInput = mView.findViewById(R.id.userInputDialog);
-
-                alertDialog
-                        .setCancelable(false)
-                        .setPositiveButton("Create", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                String directory_name = userInput.getText().toString();
-
-                                Log.d(TAG, "onClick: before insertDirectory");
-                                db.insertDirectory(directory_name);
-                                db.close();
-
-                                populateListView();
-                            }
-                        })
-                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                dialogInterface.cancel();
-                            }
-                        });
-
-                AlertDialog alertDialogAndroid = alertDialog.create();
-                alertDialogAndroid.show();
-            }
-        });
-
-
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(ctx,
-                        "This is directory " + listData.get(i) + ". Lead this to projects" +
-                                " with the directory tag.",
-                        Toast.LENGTH_LONG).show();
-
-
-            }
-        });
-
+        // Long press on listDirectory item will show context menu to edit/delete selected directory
+        registerForContextMenu(listDirectory);
 
         Log.d(TAG, "onCreate: ending");
-
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        populateListView();
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("Options");
+        menu.add(0, v.getId(), 0, "Edit");
+        menu.add(0, v.getId(), 0, "Delete");
     }
 
-    // populate list view with just directories
-    // Directories should be clickable and show page_projects/notes inside
-    private void populateListView(){
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        AdapterView.AdapterContextMenuInfo info =
+                (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        final int index = info.position;
+        final String name = listDirectory.getItemAtPosition(index).toString();
+
+        if (item.getTitle() == "Edit") {
+
+            // Open dialog for edit
+            openEditDir(name);
+
+        }
+        else if (item.getTitle() == "Delete") {
+
+            // Confirmation Dialog
+            deleteDirAlert(name);
+
+        }
+        return true;
+    }
+
+    private void populateDirectoryList(){
         Log.d(TAG, "populateListView: starting");
 
-        Cursor data = db.getAllDirectories();
-        listData = new ArrayList<>();
+        Cursor directoryCursor = db.getAllDirectories();
+        directoryData = new ArrayList<>();
 
         Log.d(TAG, "populateListView: before loop");
-        while(data.moveToNext()){
-            // index 1 should be directories column?
-            listData.add(data.getString(1));
+        while(directoryCursor.moveToNext()){
+            String directoryName = directoryCursor.getString(1);
+
+            directoryData.add(directoryName);
         }
         Log.d(TAG, "populateListView: end of loop");
-
         ListAdapter adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, listData);
-        mListView.setAdapter(adapter);
+                android.R.layout.simple_list_item_1, directoryData);
+        listDirectory.setAdapter(adapter);
 
         Log.d(TAG, "populateListView: ending");
     }
 
+    // Helper: Edit Directory
+    public void openEditDir(String oldName){
+        EditDirectory editDirectory = new EditDirectory();
+        Bundle bundle = new Bundle();
+        bundle.putString("oldName", oldName);
+        editDirectory.setArguments(bundle);
+        editDirectory.show(getSupportFragmentManager(), "edit directory");
+    }
 
+    // Helper: Delete Directory Alert Box
+    public void deleteDirAlert(final String dirName){
+        AlertDialog.Builder builder = new AlertDialog.Builder(PageDirectories.this);
+        builder.setTitle("Confirmation");
+        builder.setMessage("Are you sure you want to delete directory?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        db.deleteSingleDirectory(dirName);
+                        Toast.makeText(PageDirectories.this, "Delete", Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    // Switch to AddDirectory class
+    public void addDirectoryPage(View view){
+        Intent addDirectoryIntent = new Intent(this, AddDirectory.class);
+        startActivity(addDirectoryIntent);
+        finish();
+    }
+
+    @Override
+    public void updateName(String newName, String oldName) {
+        newDirName = newName;
+        db.updateDirectoryName(newDirName, oldName);
+        Toast.makeText(PageDirectories.this, "Updated" , Toast.LENGTH_LONG).show();
+        finish();
+    }
 
 }
