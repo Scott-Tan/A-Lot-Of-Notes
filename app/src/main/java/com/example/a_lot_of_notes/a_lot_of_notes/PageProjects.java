cd05d95c6da68bbdf55ae09f5220fc3d23a188e1
rakurnia <44179762+rakurnia@users.noreply.github.com>
2018-11-05 22:35:38 -0800

    add edit/delete functionality

--- app/src/main/java/com/example/a_lot_of_notes/a_lot_of_notes/PageProjects.java
+++ app/src/main/java/com/example/a_lot_of_notes/a_lot_of_notes/PageProjects.java
@@ -10,13 +10,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.a_lot_of_notes.a_lot_of_notes.Database.Database;
@@ -26,9 +29,11 @@ import java.util.ArrayList;
 // Directed from PageDirectories.java
 // PageProjects layout should contain page_projects
 // Directs to an individual project.
-public class PageProjects extends AppCompatActivity {
+public class PageProjects extends AppCompatActivity
+        implements EditDirectory.EditDirectoryListener{
     private static final String TAG = "PageProjects";
     static String projectPath = "";
+    String dirPath, newProjName;
     Context ctx;
     Database db;
     private ListView mListView;
@@ -40,7 +45,8 @@ public class PageProjects extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page);
-        setTitle("Projects in " + PageDirectories.directoryPath + "...");
+        dirPath = PageDirectories.directoryPath;
+        setTitle("Projects in " + dirPath + "...");
 
         Log.d(TAG, "onCreate: starting");
         ctx = this;
@@ -59,6 +65,8 @@ public class PageProjects extends AppCompatActivity {
                 AlertDialog.Builder alertDialog = new AlertDialog.Builder(ctx);
                 alertDialog.setView(mView);
                 final EditText userInput = mView.findViewById(R.id.userInputDialog);
+                TextView dialogTitle = mView.findViewById(R.id.dialogTitle);
+                dialogTitle.setText("Create a new project");
                 Log.d(TAG, "onClick: fab before alertdialog");
                 alertDialog
                         .setCancelable(false)
@@ -67,7 +75,7 @@ public class PageProjects extends AppCompatActivity {
                             public void onClick(DialogInterface dialogInterface, int i) {
                                 String project_name = userInput.getText().toString();
                                 Log.d(TAG, "onClick: before insertDirectory");
-                                db.insertProject(project_name, PageDirectories.directoryPath);
+                                db.insertProject(project_name, dirPath);
                                 populateListView();
                             }
                         })
@@ -92,13 +100,16 @@ public class PageProjects extends AppCompatActivity {
                         Toast.LENGTH_LONG).show();
                 Log.d(TAG, "onItemClick: after toast");
                 projectPath = listData.get(i);
-                PageDirectories.directoryPath = directoryData.get(i);
+                dirPath= directoryData.get(i);
 
                 Intent navToPageNotes = new Intent(ctx, PageNotes.class);
                 startActivity(navToPageNotes);
 
             }
         });
+
+        // Long press on listDirectory item will show context menu to edit/delete selected directory
+        registerForContextMenu(mListView);
 
         Log.d(TAG, "onCreate: ending");
     }
@@ -108,7 +119,7 @@ public class PageProjects extends AppCompatActivity {
     private void populateListView(){
         Log.d(TAG, "populateListView: starting");
 
-        Cursor data = db.getProjectsFromDirectory(PageDirectories.directoryPath);
+        Cursor data = db.getProjectsFromDirectory(dirPath);
         listData = new ArrayList<>();
         directoryData = new ArrayList<>();
 
@@ -126,6 +137,74 @@ public class PageProjects extends AppCompatActivity {
         mListView.setAdapter(adapter);
 
         Log.d(TAG, "populateListView: ending");
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("Options");
+        menu.add(0, v.getId(), 0, "Edit");
+        menu.add(0, v.getId(), 0, "Delete");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        AdapterView.AdapterContextMenuInfo info =
+                (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        final int index = info.position;
+        final String name = mListView.getItemAtPosition(index).toString();
+
+        if (item.getTitle() == "Edit") {
+            // Open dialog for edit
+            openEditProj(name);
+        }
+        else if (item.getTitle() == "Delete") {
+            // Confirmation Dialog
+            deleteProjAlert(name);
+        }
+        return true;
+    }
+
+    // Helper: Edit Project
+    public void openEditProj(String oldName){
+        EditDirectory editDirectory = new EditDirectory();
+        Bundle bundle = new Bundle();
+        bundle.putString("page", "Project");
+        bundle.putString("oldName", oldName);
+        editDirectory.setArguments(bundle);
+        editDirectory.show(getSupportFragmentManager(), "edit directory");
+    }
+
+    // Helper: Delete Project Alert Box
+    public void deleteProjAlert(final String projName){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Confirmation");
+        builder.setMessage("Are you sure you want to delete project?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        db.deleteSingleProject(projName,dirPath);
+                        Toast.makeText(PageProjects.this, "Delete", Toast.LENGTH_LONG).show();
+                        populateListView();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    @Override
+    public void updateName(String newName, String oldName) {
+        newProjName = newName;
+        db.updateProjectName(newProjName, oldName, dirPath);
+        Toast.makeText(PageProjects.this, "Updated" , Toast.LENGTH_LONG).show();
+        populateListView();
     }
 
 }
