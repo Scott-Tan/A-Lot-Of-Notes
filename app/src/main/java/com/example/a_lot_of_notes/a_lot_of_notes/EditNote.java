064f723087cb6867ffa96e4c75c15c9fae204b9b
rakurnia <44179762+rakurnia@users.noreply.github.com>
2018-11-14 14:14:56 -0800

    Move ShowNote function to EditNote

--- app/src/main/java/com/example/a_lot_of_notes/a_lot_of_notes/EditNote.java
+++ app/src/main/java/com/example/a_lot_of_notes/a_lot_of_notes/EditNote.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,6 +19,7 @@ public class EditNote extends AppCompatActivity {
 
     private static final String TAG = "EditNote";
     EditText editTitle, editContent;
+    Button updateNote;
     String title, content, noteID;
     Database db;
     Context ctx;
@@ -25,16 +27,35 @@ public class EditNote extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.edit_note);
+        setContentView(R.layout.add_notes);
 
         db = new Database(this);
         ctx = this;
 
-        editTitle = findViewById(R.id.edit_title_note);
-        editContent = findViewById(R.id.edit_content_note);
+        editTitle = findViewById(R.id.editText_notes_title);
+        editContent = findViewById(R.id.editText_notes_content);
+        updateNote = findViewById(R.id.button_notes_save);
         noteID = PageNotes.noteIdPath;
 
         loadNoteToEdit(noteID);
+
+        updateNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "updateNote: starting");
+
+                title = editTitle.getText().toString();
+                content = editContent.getText().toString();
+                Log.d(TAG, "updateNote: title is: " + title);
+                Log.d(TAG, "updateNote: content is: " + content);
+
+                db.updateNote(title, content, noteID);
+                Toast.makeText(EditNote.this, "Updated note with id: " + noteID,
+                        Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "updateNote: ending");
+                finish();
+            }
+        });
 
     }
 
@@ -56,19 +77,6 @@ public class EditNote extends AppCompatActivity {
         Log.d(TAG, "loadNoteToEdit: setted editText");
     }
 
-    // Update note based on the id of the current note in view
-    public void updateNote(View view) {
-        Log.d(TAG, "updateNote: starting");
+}
 
-        title = editTitle.getText().toString();
-        content = editContent.getText().toString();
-        Log.d(TAG, "updateNote: title is: " + title);
-        Log.d(TAG, "updateNote: content is: " + content);
-
-        db.updateNote(title, content, noteID);
-        Toast.makeText(this, "Updated note with id: " + noteID,
-                Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "updateNote: ending");
-        finish();
-    }
 }
