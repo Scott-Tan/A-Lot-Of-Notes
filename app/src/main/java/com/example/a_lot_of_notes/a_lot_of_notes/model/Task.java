b18e35d7f4c52ce569bb94789cfb2b3cd027cfe7
edinh21 <elizabethdinh21@gmail.com>
2018-11-23 00:34:23 -0800

    Create Task.java

---
+++ app/src/main/java/com/example/a_lot_of_notes/a_lot_of_notes/model/Task.java
@@ -0,0 +1,20 @@
+package com.example.a_lot_of_notes.a_lot_of_notes.model;
+
+import android.provider.BaseColumns;
+
+public class Task {
+    //override super constructor, no new instances allowed, on access to entries
+    private Task(){}
+
+    public static final class TaskEntry implements BaseColumns {
+        public static final String TABLE_NAME = "tasks_table";
+        public static final String COLUMN_TASK_TITLE = "task_title";
+        public static final String COLUMN_TASK_CATEGORY = "task_category";
+
+        public static String CREATE_TASKS_TABLE = "CREATE TABLE "
+                + Task.TaskEntry.TABLE_NAME + " ("
+                + Task.TaskEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + Task.TaskEntry.COLUMN_TASK_TITLE + " TEXT, "
+                + Task.TaskEntry.COLUMN_TASK_CATEGORY + " TEXT" + ")";
+    }
+}
